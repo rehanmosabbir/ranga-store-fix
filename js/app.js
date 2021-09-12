c9658c17764b5aefad77c05c1c793787a372ab95
Mosabbir Rehan <rehanmosabbir@gmail.com>
2021-09-13 00:05:48 +0600

    add rating and rating count

--- js/app.js
+++ js/app.js
@@ -22,6 +22,8 @@ const showProducts = (products) => {
         <h3>${product.title}</h3>
         <p>Category: ${product.category}</p>
         <h2>Price: $ ${product.price}</h2>
+        <p>Rating: <span><strong>${product.rating.rate}</strong></span></p>
+        <p>Total Rating: <span><strong>${product.rating.count}</strong></span></p>
         <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
         <button id="details-btn" class="btn btn-danger">Details</button>
       </div>
