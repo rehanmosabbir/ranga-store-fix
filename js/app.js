91748a44f82d7100b1907048f374b34aa3f0b4bf
Mosabbir Rehan <rehanmosabbir@gmail.com>
2021-09-12 23:43:06 +0600

    fix total price add issue in My-Cart

--- js/app.js
+++ js/app.js
@@ -35,6 +35,7 @@ const addToCart = (id, price) => {
   updatePrice("price", price);
 
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
